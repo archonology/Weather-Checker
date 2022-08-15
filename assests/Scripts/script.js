eda7f2535c8eb0b58aca929b169d48145c14c0f2
Your Name <you@example.com>
2022-08-15 00:27:27 -0500

    the click events almost ready for history buttons

--- assests/Scripts/script.js
+++ assests/Scripts/script.js
@@ -46,15 +46,20 @@ var formSubmitHandler = function (event) {
         if (response.ok) {
           response.json().then(function (data) {
             console.log(data);
-            console.log(data.list[0].weather[0].icon);
+
             //apply data to the current weather card
             displayCity.text(data.city.name);
             temp.text("Current Temp: " + data.list[0].main.temp + " F");
             wind.text("Wind: " + data.list[0].wind.speed + " mph");
-            humidity.text("Humidity: " + data.list[0].main.humidity + " %");
+            humidity.text("Humidity: " + data.list[0].main.humidity + "%");
             conditions.text(
               "Conditions: " + data.list[0].weather[0].description
             );
+            $("#featuredImage").html(
+                "<img src='https://openweathermap.org/img/wn/" +
+                  data.list[0].weather[0].icon +
+                  "@2x.png'>"
+              );
 
             //apply data to the forecast cards
             $("#image1").html(
@@ -66,7 +71,7 @@ var formSubmitHandler = function (event) {
             $("#forTemp1").text("Temp: " + data.list[1].main.temp);
             $("#forWind1").text("Wind: " + data.list[1].wind.speed + " mph");
             $("#forHumid1").text(
-              "Humidity: " + data.list[1].main.humidity + " %"
+              "Humidity: " + data.list[1].main.humidity + "%"
             );
 
             $("#image2").html(
@@ -77,7 +82,7 @@ var formSubmitHandler = function (event) {
             $("#forTemp2").text("Temp: " + data.list[2].main.temp);
             $("#forWind2").text("Wind: " + data.list[2].wind.speed + " mph");
             $("#forHumid2").text(
-              "Humidity: " + data.list[2].main.humidity + " %"
+              "Humidity: " + data.list[2].main.humidity + "%"
             );
 
             $("#image3").html(
@@ -88,7 +93,7 @@ var formSubmitHandler = function (event) {
             $("#forTemp3").text("Temp: " + data.list[3].main.temp);
             $("#forWind3").text("Wind: " + data.list[3].wind.speed + " mph");
             $("#forHumid3").text(
-              "Humidity: " + data.list[3].main.humidity + " %"
+              "Humidity: " + data.list[3].main.humidity + "%"
             );
 
             $("#image4").html(
@@ -99,7 +104,7 @@ var formSubmitHandler = function (event) {
             $("#forTemp4").text("Temp: " + data.list[4].main.temp);
             $("#forWind4").text("Wind: " + data.list[4].wind.speed + " mph");
             $("#forHumid4").text(
-              "Humidity: " + data.list[4].main.humidity + " %"
+              "Humidity: " + data.list[4].main.humidity + "%"
             );
 
             $("#image5").html(
@@ -110,18 +115,17 @@ var formSubmitHandler = function (event) {
             $("#forTemp5").text("Temp: " + data.list[5].main.temp);
             $("#forWind5").text("Wind: " + data.list[5].wind.speed + " mph");
             $("#forHumid5").text(
-              "Humidity: " + data.list[5].main.humidity + " %"
+              "Humidity: " + data.list[5].main.humidity + "%"
             );
 
             localStorage.setItem("city", cityWeather);
-            renderSearchHistory();
-            //add new city to the city array, clear the input
+            //add new city to the city array
             searchHistory.push(cityWeather);
-            cityWeather.val = "";
+            
+            //how do I clear the input?
 
-            //store updates and re-render the list
+            //store updates 
             storedCities();
-            renderSearchHistory();
           });
         } else {
           alert(
@@ -141,14 +145,129 @@ function renderSearchHistory() {
     var city = searchHistory[i];
     console.log(searchHistory);
     var btn = document.createElement("p");
-    // btn.type = "submit";
     btn.innerHTML = city;
     btn.className = "resultCity";
     btn.id = "resultCity";
-    //where to put the buttons
-    var container = document.getElementById("user-form");
-    container.appendChild(btn);
-    btn.setAttribute("data-index", i);
+
+      //where to put the buttons
+      var container = document.getElementById("user-form");
+      container.appendChild(btn);
+    //   btn.setAttribute("data-index", i);
+
+    //set up the click event
+    btn.onclick = function(){
+
+        //this for loop needs work! Almost there!
+        //
+        //
+        for (var i = 0; i < searchHistory.length; i++) {
+            var city = searchHistory[i];
+    //now do the fetch for each search history on the click
+            var historyURL =
+            "http://api.openweathermap.org/data/2.5/forecast?q=" +
+            city +
+            "&cnt=6&appid=" +
+            APIKey +
+            //imperial units sets to degrees F and MPH
+            "&units=imperial";
+        console.log(city)
+          //upcoming days via moment.js
+          $("#forecast1").text(moment().add(1, "day").format("MM/DD/YYYY"));
+          $("#forecast2").text(moment().add(2, "day").format("MM/DD/YYYY"));
+          $("#forecast3").text(moment().add(3, "day").format("MM/DD/YYYY"));
+          $("#forecast4").text(moment().add(4, "day").format("MM/DD/YYYY"));
+          $("#forecast5").text(moment().add(5, "day").format("MM/DD/YYYY"));
+        
+          if (city) {
+            fetch(historyURL)
+              .then(function (response) {
+                if (response.ok) {
+                  response.json().then(function (data) {
+                    console.log(data);
+                    console.log(data.list[0].weather[0].icon);
+                    //apply data to the current weather card
+                    displayCity.text(data.city.name);
+                    temp.text("Current Temp: " + data.list[0].main.temp + " F");
+                    wind.text("Wind: " + data.list[0].wind.speed + " mph");
+                    humidity.text("Humidity: " + data.list[0].main.humidity + "%");
+                    conditions.text(
+                      "Conditions: " + data.list[0].weather[0].description
+                    );
+                    $("#featuredImage").html(
+                        "<img src='https://openweathermap.org/img/wn/" +
+                          data.list[0].weather[0].icon +
+                          "@2x.png'>"
+                      );
+        
+                    //apply data to the forecast cards
+                    $("#image1").html(
+                      "<img src='https://openweathermap.org/img/wn/" +
+                        data.list[1].weather[0].icon +
+                        "@2x.png'>"
+                    );
+                    $("#forecast1").text(data.list[1].timezone);
+                    $("#forTemp1").text("Temp: " + data.list[1].main.temp);
+                    $("#forWind1").text("Wind: " + data.list[1].wind.speed + " mph");
+                    $("#forHumid1").text(
+                      "Humidity: " + data.list[1].main.humidity + "%"
+                    );
+        
+                    $("#image2").html(
+                      "<img src='https://openweathermap.org/img/wn/" +
+                        data.list[2].weather[0].icon +
+                        "@2x.png'>"
+                    );
+                    $("#forTemp2").text("Temp: " + data.list[2].main.temp);
+                    $("#forWind2").text("Wind: " + data.list[2].wind.speed + " mph");
+                    $("#forHumid2").text(
+                      "Humidity: " + data.list[2].main.humidity + "%"
+                    );
+        
+                    $("#image3").html(
+                      "<img src='https://openweathermap.org/img/wn/" +
+                        data.list[3].weather[0].icon +
+                        "@2x.png'>"
+                    );
+                    $("#forTemp3").text("Temp: " + data.list[3].main.temp);
+                    $("#forWind3").text("Wind: " + data.list[3].wind.speed + " mph");
+                    $("#forHumid3").text(
+                      "Humidity: " + data.list[3].main.humidity + "%"
+                    );
+        
+                    $("#image4").html(
+                      "<img src='https://openweathermap.org/img/wn/" +
+                        data.list[4].weather[0].icon +
+                        "@2x.png'>"
+                    );
+                    $("#forTemp4").text("Temp: " + data.list[4].main.temp);
+                    $("#forWind4").text("Wind: " + data.list[4].wind.speed + " mph");
+                    $("#forHumid4").text(
+                      "Humidity: " + data.list[4].main.humidity + "%"
+                    );
+        
+                    $("#image5").html(
+                      "<img src='https://openweathermap.org/img/wn/" +
+                        data.list[5].weather[0].icon +
+                        "@2x.png'>"
+                    );
+                    $("#forTemp5").text("Temp: " + data.list[5].main.temp);
+                    $("#forWind5").text("Wind: " + data.list[5].wind.speed + " mph");
+                    $("#forHumid5").text(
+                      "Humidity: " + data.list[5].main.humidity + "%"
+                    );
+                  });
+                } else {
+                  alert(
+                    "Error: check spelling or input format(City name only or City, State initials, USA)"
+                  );
+                }
+              })
+              .catch(function (error) {
+                alert("Unable to connect to OpenWeatherMap");
+              });
+          }
+        };
+    }
   }
 }
 
@@ -173,4 +292,4 @@ function storedCities() {
 //gets the click for the submit button
 userFormEl.addEventListener("submit", formSubmitHandler);
 //calls init to retriev data and render it to teh page on load
-init();
+init()
